8a538e52eb50253ebfdd3182017def6af060bc17
xiboke <zhangwenchao@ezparking.com.cn>
2017-10-13 14:25:08 +0800

    first commit

---
+++ sample/src/main/java/org/zackratos/sample/SampleDialog.java
@@ -0,0 +1,27 @@
+package org.zackratos.sample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.zackratos.bottomdialog.BottomDialog;
+
+/**
+ *
+ * Created by xiboke on 2017/10/13.
+ */
+
+public class SampleDialog extends BottomDialog {
+
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container) {
+        View view = inflater.inflate(R.layout.dialog_sample, container, false);
+        view.findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+        return view;
+    }
+}
